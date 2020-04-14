e757949249499fd283f022785bc2a9cbc34533f4
Guy Davidson <guydav@gmail.com>
2020-04-14 13:35:30 -0400

    Minor changes

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -2,6 +2,7 @@
 
 image="craftassist"
 shell="/bin/bash"
+default_flags="-p 25565:25565 -e CRAFTASSIST_NO_UPLOAD=1 "
 
 target_script=$1
 docker_run_flags=${@:2}
@@ -11,7 +12,7 @@ b64=`gzip -c ${target_script} | base64`
 # output=`echo "${b64}" |  base64 --decode | gunzip -c`
 #echo $output
 
-docker run -t -e RUN_SH_GZ_B64=${b64} ${docker_run_flags} ${image} ${shell}
+docker run -t -e RUN_SH_GZ_B64=${b64} ${default_flags} ${docker_run_flags} ${image} ${shell}
 
 
 
